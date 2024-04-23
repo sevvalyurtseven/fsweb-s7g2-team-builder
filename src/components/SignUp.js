88325261f267aba271974b92c14e9a5d6b20a952
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-23 19:37:24 +0300

    create signup page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,40 @@
-const SignUp = () => {
+import { Form, FormGroup } from "reactstrap";
+
+const SignUp = ({ submitHandler, changeHandler, formData }) => {
   return (
-    <div>
-      <h1>SignUp</h1>
-    </div>
+    <Form onSubmit={submitHandler}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          placeholder="İsim Giriniz"
+          value={formData.name}
+          onChange={changeHandler}
+        />
+      </label>
+      <label>
+        Rol
+        <input
+          type="text"
+          name="rol"
+          placeholder="Rol Giriniz"
+          value={formData.rol}
+          onChange={changeHandler}
+        />
+      </label>
+      <label>
+        E-mail
+        <input
+          type="email"
+          name="email"
+          placeholder="johndoe@me.com"
+          value={formData.email}
+          onChange={changeHandler}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </Form>
   );
 };
 export default SignUp;
