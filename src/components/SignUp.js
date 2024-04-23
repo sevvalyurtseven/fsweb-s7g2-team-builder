78f00a7d1372ae0790c333bc514cbdd4b2359efb
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-23 19:58:44 +0300

    css detail

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,40 +1,55 @@
-import { Form, FormGroup } from "reactstrap";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 
 const SignUp = ({ submitHandler, changeHandler, formData }) => {
   return (
-    <Form onSubmit={submitHandler}>
-      <label>
-        Name
-        <input
-          type="text"
-          name="name"
-          placeholder="İsim Giriniz"
-          value={formData.name}
-          onChange={changeHandler}
-        />
-      </label>
-      <label>
-        Rol
-        <input
-          type="text"
-          name="rol"
-          placeholder="Rol Giriniz"
-          value={formData.rol}
-          onChange={changeHandler}
-        />
-      </label>
-      <label>
-        E-mail
-        <input
-          type="email"
-          name="email"
-          placeholder="johndoe@me.com"
-          value={formData.email}
-          onChange={changeHandler}
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </Form>
+    <div style={{ padding: "40px" }}>
+      <h1 style={{ textAlign: "center" }}>Sign Up Form</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: "100vh",
+          padding: "50px",
+        }}
+      >
+        <Form onSubmit={submitHandler}>
+          <FormGroup>
+            <Label>Name</Label>
+            <Input
+              type="text"
+              name="name"
+              placeholder="İsim Giriniz"
+              value={formData.name}
+              onChange={changeHandler}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Rol</Label>
+            <Input
+              type="text"
+              name="rol"
+              placeholder="Rol Giriniz"
+              value={formData.rol}
+              onChange={changeHandler}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>E-mail</Label>
+            <Input
+              type="email"
+              name="email"
+              placeholder="johndoe@me.com"
+              value={formData.email}
+              onChange={changeHandler}
+            />
+          </FormGroup>
+
+          <Button color="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 };
 export default SignUp;
