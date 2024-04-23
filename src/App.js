11d5b15737f4f7524cf07a4d0262cd58630690e4
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-23 10:08:23 +0300

    create helper functions and states

--- src/App.js
+++ src/App.js
@@ -2,8 +2,32 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Members from "./components/Members";
 import SignUp from "./components/SignUp";
+import { useState } from "react";
 
 function App() {
+  const membersInitials = [
+    { name: "John", email: "john@example.com", rol: "Frontend Developer" },
+    { name: "Jane", email: "jane@example.com", rol: "Frontend Developer" },
+    { name: "Jim", email: "jim@example.com", rol: "Frontend Developer" },
+  ];
+  const formDataInitials = {
+    name: "",
+    email: "",
+    rol: "",
+  };
+  const [formData, setFormData] = useState(formDataInitials);
+  const [members, setMembers] = useState(membersInitials);
+
+  const submitHandler = () => {
+    setMembers([...members, formData]);
+  };
+
+  const changeHandler = (event) => {
+    //Destructure
+    let { value, type, name, checked } = event.target;
+    value = type === "checkbox" ? checked : value;
+    setFormData({ ...formData, [name]: value });
+  };
   return (
     <div>
       <Switch>
@@ -11,7 +35,7 @@ function App() {
           <Members />
         </Route>
         <Route path="/signup" exact>
-          <SignUp />
+          <SignUp submitHandler={submitHandler} changeHandler={changeHandler} />
         </Route>
       </Switch>
     </div>
