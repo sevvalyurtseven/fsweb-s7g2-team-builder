31933f6bed2dcc016f6a5608ccd95d578a155269
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-23 11:07:59 +0300

    create members card component

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,7 +1,43 @@
-const Members = () => {
+import {
+  Button,
+  Card,
+  CardBody,
+  CardImg,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+} from "reactstrap";
+
+const Members = ({ members }) => {
+  //console.log(members);
   return (
-    <div>
-      <h1>Members</h1>
+    <div style={{ textAlign: "center" }}>
+      {members.map((member) => (
+        <Card
+          style={{
+            width: "20rem",
+            display: "inline-block",
+            margin: "10px",
+            padding: "10px",
+            textAlign: "center",
+            border: "1px solid black",
+            cursor: "pointer",
+            borderRadius: "10px",
+            boxShadow: "5px 5px 10px #ccc",
+            transition: "all 0.3s ease-in-out",
+          }}
+        >
+          <CardImg variant="top" src={member.img} />
+          <CardBody>
+            <CardTitle tag="h5">{member.name}</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">
+              {member.rol}
+            </CardSubtitle>
+            <CardText>{member.email}</CardText>
+            <Button color="primary">Detay</Button>
+          </CardBody>
+        </Card>
+      ))}
     </div>
   );
 };
